ee13622cd0d3c1c5fc32a05add0ca61c923b0f7f
Bruno de Souza Valeiro <bvaleiro85@hotmail.com>
2019-11-30 07:47:33 -0300

    app quase pronto

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -3,6 +3,6 @@ const local = 'http://192.168.137.1:3001';
 const remoto = 'https://backendcolisao.herokuapp.com';
 
 const Api = axios.create({
-	baseURL:local,
+	baseURL:remoto,
 })
 export default Api;
